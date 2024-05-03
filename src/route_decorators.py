f03b933f7aca8a665065f0fbb97bc3be5e01e720
Eol <eol@eolstudy.com>
2024-05-03 18:32:15 +0900

    new file: route_decorators.py to place decorators
        body_requires: decorator to check missing request body items

---
+++ src/route_decorators.py
@@ -0,0 +1,25 @@
+from functools import wraps
+
+from flask import request
+
+
+# Return a decorator that makes sure no item is missing before invoking func
+# Generate 406 if missing anything
+def body_requires(itemList: list[str]):
+    def decorator(func):
+        @wraps(func)
+        def new_func(*args, **kwargs):
+            missedItemList = []
+            data = request.get_json()
+            for item in itemList:
+                if data.get(item) is None:
+                    missedItemList.append(item)
+
+            if missedItemList:
+                return {"missing": missedItemList}, 406
+            else:
+                return func(*args, **kwargs)
+
+        return new_func
+
+    return decorator
