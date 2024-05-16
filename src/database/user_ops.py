2f322fb363602aa3f19c9e31ab7a812a3123f482
Hoshi0111 <hoshito.n0111@gmail.com>
2024-05-16 17:17:40 +0900

    add name

--- src/database/user_ops.py
+++ src/database/user_ops.py
@@ -56,6 +56,7 @@ def get_user_full(userId: int) -> dict:
         'user_id': baseUser.id,
         "email": baseUser.Email,
         "language": baseUser.Language_code,
+        "name" : baseUser.Name
     }
 
     if baseUser.Type == PATIENT:
