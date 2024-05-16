444b35acca5f5a7c3c038429d87da1f9217ebafa
Eol <eol@eolstudy.com>
2024-05-16 16:40:47 +0900

    M websocket: auth structure

--- src/websocket.py
+++ src/websocket.py
@@ -1,1 +1,33 @@
+from datetime import datetime
+
+from flask import request
+from flask_socketio import emit, disconnect
+
 from . import socketio
+from . import database as db
+
+@socketio.on('connect')
+def connect():
+    print(f'New client connected: {request.sid}')
+
+@socketio.on('disconnect')
+def disconnect():
+    print(f'Client disconnected: {request.sid}')
+
+@socketio.on('auth')
+def auth(json):
+    token = json.get('token')
+    roomId = json.get('roomId')
+
+    try:
+        user = db.user.get_user_by_token(token)
+    except Exception:
+        user = None
+    finally:
+        if user is None or user['expirationTime'] < datetime.now():
+            emit('auth', {
+                'error': 'Forbidden'
+            })
+
+    emit('auth', user)
+
