da9ea4112f0f502484ee318e0c57ebd404f40e1a
Eol <eol@eolstudy.com>
2024-05-17 16:31:44 +0900

    M ws: get_session decorator

--- src/websocket.py
+++ src/websocket.py
@@ -12,11 +12,33 @@ from . import database as db
 
 wsSessions = []
 
+
+def get_session(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        global wsSessions
+        # get session from wsSession by session[sid]
+        sid = request.sid
+        try:
+            session = list(filter(lambda session : session['sid'] == sid, wsSessions))[0]
+        except IndexError:
+            emit ('error', {
+                'error': 'InternalServerError',
+                'message': 'User is authenticated. However, no registered sid is found.'
+            })
+            disconnect()
+            return
+
+        return func(session, *args, **kwargs)
+
+    return inner
+
+
 @socketio.on('connect')
 def connect(auth):
     token = auth.get('token')
     roomId = auth.get('roomId')
-    sid = request.sid
+    sid: str = request.sid
 
     unauthError = {
         'error': 'unauthorizationError'
@@ -27,7 +49,7 @@ def connect(auth):
         user = db.user.get_user_by_token(token)
         if user and user['expirationTime'] < datetime.now():
             user = None
-    except Exception:
+    except DoesNotExist:
         user = None
 
     if user is None:
@@ -39,7 +61,7 @@ def connect(auth):
     # Check if in room
     try:
         rooms = db.room_op.get_rooms_all(user['id'])['rooms']
-        roomIds = list(map(lambda room : room.get('roomId'), rooms))
+        roomIds = list(map(lambda room: room.get('roomId'), rooms))
 
     except DoesNotExist:
         roomIds = []
@@ -62,3 +84,9 @@ def on_disconnect():
     print(f'User disconnected: {request.sid}')
     global wsSessions
     wsSessions = [session for session in wsSessions if session['sid'] != request.sid]
+
+
+@socketio.on('message')
+@get_session
+def message(data):
+    pass
