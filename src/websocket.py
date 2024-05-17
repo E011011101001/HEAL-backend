291077fc15112195b7fe27eacde75e29ac4e7035
Eol <eol@eolstudy.com>
2024-05-17 18:55:14 +0900

    M ws: message logic in English

--- src/websocket.py
+++ src/websocket.py
@@ -88,5 +88,18 @@ def on_disconnect():
 
 @socketio.on('message')
 @get_session
-def message(data):
-    pass
+def message(session, json):
+    """
+    1. Get room stage.
+    2. If there is no active doctor in the room, stage = 1, and message sent to chatbot;
+    3. If there is at least one active doctor in the room, stage = 2, and message sent to socketio room.
+    4. If the other user in the room speaks a different language, send translation along with the message.
+
+    For all the messages sending to the front end:
+    1. Get terms by GPT.
+    2. If terms exists in the database, send the data stored in the database.
+    3. If terms does not exist, ask GPT for information and possible wiki links of the term. Send the data, and
+        store them in the database.
+    4. After 2&3, store the terms in the chat history
+
+    """
