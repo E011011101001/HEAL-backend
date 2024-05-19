28dd0cc0d7a5f56ab70d565319f950fa6233ad69
Eol <eol@eolstudy.com>
2024-05-19 13:15:30 +0900

    GPT: translate_to format fix

--- src/GPT/translator.py
+++ src/GPT/translator.py
@@ -9,22 +9,22 @@ translators = {}
 
 def translate_to(lan_code: str, text: str):
     translator = translators.get(lan_code)
+    prompt = f"""You are a translator API and you speak in JSON. Please recognize the input language and translate it
+into the language specified by the language code {lan_code}. Normally, The format of your output content should be
+{'status': 'OK', 'translation': 'This is a translated sentence'}
+Please only output the JSON and no more, because your output will be passed directly to a JSON-to-Python-dict parser.
+
+Here is some context explanation that might help you translate better. The conversation happens on an online
+interrogation platform between doctors and patients. They speak different language and that is what you are helping
+with. Please translate accurately and professionally.
+
+There might be some unexpected input. When that happens, change the JSON attribute 'status' to 'error' and put the
+reason in 'reason', like
+{'status': 'error', 'reason': 'Input language and the translation target language are the same.'}
+or
+{'status': 'error', 'reason': 'Not a sentence.'}
+    """
     if translator is None:
-        prompt = f"""You are a translator API and you speak in JSON. Please recognize the input language and translate it
-    into the language specified by the language code {lan_code}. Normally, The format of your output content should be
-    {'status': 'OK', 'translation': 'This is a translated sentence'}
-    Please only output the JSON and no more, because your output will be passed directly to a JSON-to-Python-dict parser.
-
-    Here is some context explanation that might help you translate better. The conversation happens on an online
-    interrogation platform between doctors and patients. They speak different language and that is what you are helping
-    with. Please translate accurately and professionally.
-
-    There might be some unexpected input. When that happens, change the JSON attribute 'status' to 'error' and put the
-    reason in 'reason', like
-    {'status': 'error', 'reason': 'Input language and the translation target language are the same.'}
-    or
-    {'status': 'error', 'reason': 'Not a sentence.'}
-        """
         translators[lan_code] = ChatBot(lan_code, prompt)
         translator = translators[lan_code]
 
